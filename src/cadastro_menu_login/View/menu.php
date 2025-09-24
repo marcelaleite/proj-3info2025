9ed35bda3ffa8d3df4e2e030932d940f8fa60002
Brendadesouza <desouzab211@gmail.com>
2025-09-24 16:32:22 -0300

    menu

--- src/cadastro_menu_login/View/menu.php
+++ src/cadastro_menu_login/View/menu.php
@@ -104,7 +104,6 @@
           <div class="mc-meta"><span class="badge ready">Template</span></div>
         </a>
 
-<<<<<<< HEAD:src/cadastro_menu_login/View/menu.html
 
        
         <a class="module-card" href="../../galeria/View/galeria.php" aria-label="Galeria">
@@ -113,13 +112,6 @@
           <div class="mc-meta">
             <span class="badge ready">Template</span> 
           </div>
-=======
-        <!-- Galeria -->
-        <a class="module-card module-create" href="novo-modulo.html" aria-label="Criar novo módulo">
-          <div style="font-size:22px;">Galeria</div>
-          <div>Registro de características hereditárias.</div>
-          <div class="mc-meta"><span class="badge dev">Em dev</span></div>
->>>>>>> 470f30a664e9c0cb9b65eec70a1ba39a9e8a5ed0:src/cadastro_menu_login/View/menu.php
         </a>
       </nav>
     </div>
